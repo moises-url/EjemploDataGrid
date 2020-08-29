e8ed6946a25ee2528e1a9fc846ece0c07ac02f0f
moises-url <moises.alonso.catedras@url.edu.gt>
2020-08-29 10:18:59 -0600

    Ejemplo rollback

--- EjemploDataGrid/MyForm.h
+++ EjemploDataGrid/MyForm.h
@@ -369,8 +369,7 @@ private: System::Void btnExportar_Click(System::Object^ sender, System::EventArg
 }
 private: System::Void btnNuevaVentana_Click(System::Object^ sender, System::EventArgs^ e) {
 	TitleForm^ nuevaVentana = gcnew TitleForm();
-	nuevaVentana->AsignarTitulo(txtNuevaVentana->Text);
-	nuevaVentana->ShowDialog();
+
 }
 };
 }
